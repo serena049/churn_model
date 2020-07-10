49c0e7b449f7bba31617c154d33a36afc5b58241
Wei (Serena) Zou <serena049@gmail.com>
2020-07-10 16:56:59 -0400

    fix the bugs in sphnix

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-    Setup file for churn_model_project.
+    Setup file for churn_model.
     Use setup.cfg to configure your project.
 
     This file was generated with PyScaffold 3.2.3.
