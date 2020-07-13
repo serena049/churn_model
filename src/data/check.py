d4d1cb2f32b644d07fa4ca52df771ca66a31d24f
Wei (Serena) Zou <serena049@gmail.com>
2020-07-12 23:17:23 -0400

    modify data module

--- src/data/check.py
+++ src/data/check.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-# import argparse
-# import sys
-# import logging
-
-from pathlib import Path
+import argparse
+import sys
+import logging
+import pathlib as pl
+import pandas as pd
+import time
+import numpy as np
 
 __author__ = "Wei (Serena) Zou"
 __copyright__ = "Wei (Serena) Zou"
@@ -12,105 +14,104 @@ __license__ = "mit"
 
 # _logger = logging.getLogger(__name__)
 
-class Check():
+class Check:
     """
     Class to check path, data size and data type
     """
+    #
+    def __init__(self, data_path):
+        self.data_path = data_path
 
-    def __init__(self):
-        pass
+    def list_all_files(self) -> pd.DataFrame:
+        """
+        go through all the files in the data_analysis directory and build a DataFrame with the file name,
+        parent path and modified time.
 
-    def check_path(self):
-        return
+        Returns: a pandas df
 
-    def check_size(self):
-        return
+        """
+
+        all_files = []
+        for i in self.data_path.rglob('*.*'):
+            all_files.append((i.name, i.parent, time.ctime(i.stat().st_ctime)))
+
+        columns = ["File_Name", "Parent", "Created"]
+        df = pd.DataFrame.from_records(all_files, columns=columns)
+
+        return df
+
+    @staticmethod
+    def check_path(df):
+        """
+        Check if the data folder is empty
+        Args:
+            df: this is the pandas df listing all the files in the folder
+
+        Returns: raise ValueError is the folder is empty
+        """
+        if len(df.index) >= 1:
+            print("input file exists.")
+        else:
+            raise ValueError("empty folder!")
+
+    @staticmethod
+    def load(df: pd.DataFrame, file_col_name: str = "File_Name", time_col_name: str = "Created",
+             parent_path_col_name: str = "Parent", contains_str: str = "datasets"):
+
+        """
+        Find the most recent data file and load the data
+        Args:
+            df: pandas df with all the files in the directory
+            file_col_name: column name of file names in df
+            time_col_name: column name of created time in df
+            parent_path_col_name: column name of parent path in df
+            contains_str: data input file should have this str in the file name
+
+        Returns: pandas df of input data
+        """
+
+        # select all the files containing name "datasets"
+        files = df[df[file_col_name].str.contains(contains_str, na=False)]
+        if len(files.index) >= 1:
+            # select the most recent data
+            file_path = files.sort_values(time_col_name, ascending=False)[parent_path_col_name][0].joinpath(
+                        files.sort_values(time_col_name, ascending=False)[file_col_name][0])
+            # load data
+            data = pd.read_csv(file_path)
+
+            # standardize all column names to lowercase
+            data.columns = map(str.lower, data.columns)
+
+            return data
+        else:
+            raise ValueError("file not exists!")
+
+    @staticmethod
+    def check_size(data: pd.DataFrame, n_cols: int = 21):
+        """
+        Check # of columns and if the data is empty
+        Args:
+            data: input data
+            n_cols: reference of the number of columns
+
+        Returns: pass or raise error
+
+        """
+        if (len(data.index) > 0) and (len(data.columns) == n_cols):
+            print("data size correct.")
+        else:
+            raise ValueError("data size wrong!")
 
     def check_type(self):
         return
 
-# def fib(n):
-#     """Fibonacci example function
-#
-#     Args:
-#       n (int): integer
-#
-#     Returns:
-#       int: n-th Fibonacci number
-#     """
-#     assert n > 0
-#     a, b = 1, 1
-#     for i in range(n-1):
-#         a, b = b, a+b
-#     return a
-#
-#
-# def parse_args(args):
-#     """Parse command line parameters
-#
-#     Args:
-#       args ([str]): command line parameters as list of strings
-#
-#     Returns:
-#       :obj:`argparse.Namespace`: command line parameters namespace
-#     """
-#     parser = argparse.ArgumentParser(
-#         description="Just a Fibonacci demonstration")
-#     parser.add_argument(
-#         "--version",
-#         action="version",
-#         version="churn_model_project {ver}".format(ver=__version__))
-#     parser.add_argument(
-#         dest="n",
-#         help="n-th Fibonacci number",
-#         type=int,
-#         metavar="INT")
-#     parser.add_argument(
-#         "-v",
-#         "--verbose",
-#         dest="loglevel",
-#         help="set loglevel to INFO",
-#         action="store_const",
-#         const=logging.INFO)
-#     parser.add_argument(
-#         "-vv",
-#         "--very-verbose",
-#         dest="loglevel",
-#         help="set loglevel to DEBUG",
-#         action="store_const",
-#         const=logging.DEBUG)
-#     return parser.parse_args(args)
-#
-#
-# def setup_logging(loglevel):
-#     """Setup basic logging
-#
-#     Args:
-#       loglevel (int): minimum loglevel for emitting messages
-#     """
-#     logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-#     logging.basicConfig(level=loglevel, stream=sys.stdout,
-#                         format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
-#
-#
-# def main(args):
-#     """Main entry point allowing external calls
-#
-#     Args:
-#       args ([str]): command line parameter list
-#     """
-#     args = parse_args(args)
-#     setup_logging(args.loglevel)
-#     _logger.debug("Starting crazy calculations...")
-#     print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
-#     _logger.info("Script ends here")
-#
-#
-# def run():
-#     """Entry point for console_scripts
-#     """
-#     main(sys.argv[1:])
-#
-#
-# if __name__ == "__main__":
-#     run()
+
+if __name__ == "__main__":
+    input_data_path = pl.Path(__file__).resolve().parents[2].joinpath('data/input/') # the parent directory of current script
+    check_data = Check(input_data_path)
+    df_all_files = check_data.list_all_files()
+    check_data.check_path(df_all_files)
+    df_raw = check_data.load(df_all_files)
+    check_data.check_size(df_raw)
+
+
