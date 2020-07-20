b99ad6e09543d8a2facb07e8fbe4f7cd8768ab5e
Wei (Serena) Zou <serena049@gmail.com>
2020-07-20 14:16:39 -0400

    create 3 new folders, src for python codes, tests for tests and data for input and output data

---
+++ tests/data/test_preprocessing_helpers.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+# from data.preprocessing_helpers import DataProcess
+
+class TestLoadData():
+
+    def test_path_exist(self):
+        pass
+
+    def test_data_size(self):
+        pass
---
+++ tests/features/test_as_numpy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+# from data.preprocessing_helpers import DataProcess
+
+class TestLoadData():
+
+    def test_path_exist(self):
+        pass
+
+    def test_data_size(self):
+        pass
+
