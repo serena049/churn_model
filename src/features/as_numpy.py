839e5d0304498f8a704f444214881a802d207f03
Wei (Serena) Zou <serena049@gmail.com>
2020-07-20 14:03:36 -0400

    commit all

--- src/features/as_numpy.py
+++ src/features/as_numpy.py
@@ -1,4 +1,69 @@
 # -*- coding: utf-8 -*-
 
-class AsNumpy():
-    pass
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.metrics import roc_auc_score, roc_curve, scorer
+from sklearn.metrics import f1_score
+import statsmodels.api as sm
+from sklearn.metrics import precision_score, recall_score
+
+
+
+
+class TrainTestSplit:
+
+    # splitting train and test data
+    def __init__(self, data, target_col: str='churn', id_col: str='customerid'):
+
+        self.data = data
+        self.target_col = target_col
+        self.id_col = id_col
+
+    def train_test_split(self):
+        train, test = train_test_split(self.data, test_size=0.25, random_state=111)
+
+        # seperating dependent and independent variables
+        all_cols = self.data.columns
+        id_col = self.id_col
+        target_col = self.target_col
+        cols = [i for i in all_cols if i not in id_col + target_col]
+        train_x = train[cols].values
+        train_y = train[target_col].values
+        test_x = test[cols].values
+        test_y = test[target_col].values
+
+        return train_x, train_y, test_x, test_y, cols
+
+
+    @staticmethod
+    def churn_prediction(self, algorithm, training_x, testing_x, training_y, testing_y, cols, cf, threshold_plot):
+
+        # model
+        algorithm.fit(training_x, training_y)
+        predictions = algorithm.predict(testing_x)
+        probabilities = algorithm.predict_proba(testing_x)
+
+        # coeffs (coef for logistic regression , feature importance for tree models)
+        if cf == "coefficients":
+            coefficients = pd.DataFrame(algorithm.coef_.ravel())
+        elif cf == "features":
+            coefficients = pd.DataFrame(algorithm.feature_importances_)
+        column_df = pd.DataFrame(cols)
+        coef_summary = (pd.merge(coefficients, column_df, left_index=True, right_index=True, how="left"))
+        coef_summary.columns = ["coefficients", "features"]
+        coef_summary = coef_summary.sort_values(by="coefficients", ascending=False)
+
+        # evaluation
+        report = classification_report(testing_y, predictions)
+        accuracy = accuracy_score(testing_y, predictions)
+
+        # confusion matrix
+        conf_matrix = confusion_matrix(testing_y, predictions)
+
+        # roc_auc_score
+        model_roc_auc = roc_auc_score(testing_y, predictions)
+        fpr, tpr, thresholds = roc_curve(testing_y, probabilities[:, 1])
+
+        return coef_summary, report, accuracy, conf_matrix, model_roc_auc,fpr, tpr, thresholds
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
-from churn_model.src.data import check
+from data.check import Check
+
+import pathlib as pl
 
 __author__ = "Wei (Serena) Zou"
 __copyright__ = "Wei (Serena) Zou"
@@ -12,18 +14,18 @@ __license__ = "mit"
 
 if __name__ == "__main__":
     # load
-    input_data_path = pl.Path(__file__).resolve().parents[2].joinpath('data/input/') # the parent directory of current script
+    input_data_path = pl.Path(__file__).resolve().parents[2].joinpath(
+        'data/input/')  # the parent directory of current script
+    print(input_data_path)
     check_data = check.Check(input_data_path)
-    df_all_files = check_data.list_all_files()
-    check_data.check_path(df_all_files)
-    df_raw = check_data.convert(df_all_files)
-    df_raw = check_data.load(df_all_files)
-
-    # transformation
-    df_transformation = check.Transformation(df_raw)
-    df_rv_na = df_transformation.remove_missing_value()
-
-    target, df_encode = df_transformation.preprocess(df_rv_na)
-    print(df_encode)
-    print(target)
-
+    # df_all_files = check_data.list_all_files()
+    # check_data.check_path(df_all_files)
+    # df_raw = check_data.convert(df_all_files)
+    # df_raw = check_data.load(df_all_files)
+    #
+    # # transformation
+    # df_transformation = check.Transformation(df_raw)
+    # df_rv_na = df_transformation.remove_missing_value()
+    #
+    # target, df_encode = df_transformation.preprocess(df_rv_na)
+    # print(df_encode)
